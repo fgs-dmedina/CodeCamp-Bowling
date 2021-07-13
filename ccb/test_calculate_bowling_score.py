239f13b1a2cea66f9c52454d87b3a2518d5567ce
Daniel Medina <dmedina@foxguardsolutions.com>
2021-07-13 15:47:49 -0400

    fix: add condition to handle second roll spares
    Add condition to handle second roll spares.
    Account for first roll misses using an if / else
    statement to assign previous rolls.

--- ccb/test_calculate_bowling_score.py
+++ ccb/test_calculate_bowling_score.py
@@ -11,3 +11,21 @@ def test_all_non_spares_returns_ninety():
 
 def test_fives_with_spares_returns_one_fifty():
     assert calculate_bowling_score('5/5/5/5/5/5/5/5/5/5/5') == 150
+
+def test_two_non_strikes_returns_two_eighty_five():
+    assert calculate_bowling_score('XXXXXXXXXX5/') == 285
+
+def test_tenth_frame_no_strike_returns_two_sixty_seven():
+    assert calculate_bowling_score('XXXXXXXXX7/-') == 267
+
+def test_all_strikes_returns_perfect_score():
+    assert calculate_bowling_score('XXXXXXXXXXXX') == 300
+
+def test_all_strikes_miss_last_frame_returns_two_forty():
+    assert calculate_bowling_score('XXXXXXXXX--') == 240
+
+def test_final_roll_as_strike_returns_two_seventy_nine():
+    assert calculate_bowling_score('XXXXXXXXX9/X') == 279
+
+def test_strikes_as_spares_returns_one_ten():
+    assert calculate_bowling_score('X-/-/-/--X-/X--X--') == 110
