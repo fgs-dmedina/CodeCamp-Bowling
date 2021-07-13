b929f3a2649db0baa4e606df3cbe90458e243601
Daniel Medina <dmedina@foxguardsolutions.com>
2021-07-13 13:42:39 -0400

    feat:  implement calculation for spares
    Calculate scores with spares using original
    input for reference, retrieve the next roll,
    add it to our game's score, and increase
    the number of frames by 1.  This is a simple
    workaround that avoids duplicate calculations
    or adding scores between frames.

--- ccb/calculate_bowling_score.py
+++ ccb/calculate_bowling_score.py
@@ -2,25 +2,53 @@ from convert_string_to_integer_list import convert_string_to_integer_list
 
 def calculate_bowling_score(rolls: str) -> int:
 
-    ALL_PINS = 10
+    original_input = rolls
+    ALL_PINS = int(10)
 
-    game_score = 0
-    perfect_score = 300
+    game_score = int(0)
+    perfect_score = int(300)
 
-    all_rolls = convert_string_to_integer_list(rolls)
-    print(all_rolls)
-
-    did_player_have_perfect_game = rolls == 'XXXXXXXXXXXX'
+    did_player_have_perfect_game = original_input == 'XXXXXXXXXXXX'
     if did_player_have_perfect_game:
         return perfect_score
 
-    for i, roll in enumerate(rolls):
-        pass
+    all_rolls = convert_string_to_integer_list(rolls)
+    
+    frame = 1
 
+    for i, roll in enumerate(all_rolls):
+        
+        if frame == 11:
+            break
 
+        is_roll_spare = original_input[i] == '/'
+        is_roll_strike = roll == ALL_PINS
+
+        is_second_last_roll = roll == all_rolls[-2]
+        is_final_roll = roll == all_rolls[-1]
+
+        if is_roll_strike:
+
+            strike_bonus = int(0)
+
+            if is_second_last_roll or is_final_roll:
+                strike_bonus = ALL_PINS
+            else:
+                strike_bonus = int(all_rolls[i + 1] + all_rolls[i + 2])
+
+            game_score += strike_bonus
+            frame += 1
+            
+        elif is_roll_spare:
+            spare_bonus = all_rolls[i + 1]
+            game_score += spare_bonus
+            frame += 1
+
+        game_score += roll
 
     print('Final game score: ' + str(game_score))
     return game_score
+
 
 if __name__ == '__main__':
     calculate_bowling_score('5/5/5/5/5/5/5/5/5/5/5')
